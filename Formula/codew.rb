03c557fb949d783f43a9a0297f88f6213813efea
shuheikikuchi <qu1dpr09u0@gmail.com>
2024-02-01 20:29:29 +0900

    update codew

--- Formula/codew.rb
+++ Formula/codew.rb
@@ -1,8 +1,8 @@
 class Codew < Formula
   desc "Open folder as a vscode multi-root workspace"
-  homepage "https://github.com/bisquit/codew-deno"
-  url "https://github.com/bisquit/codew-deno/releases/download/v0.0.1/codew-0.0.1-x86_64-apple-darwin.tar.gz"
-  sha256 "b90ebbc345b04cdea9766f3770c59afb6f8f12e48169dbbdb3f0193dae11c744"
+  homepage "https://github.com/bisquit/codew-cli"
+  url "https://github.com/bisquit/codew-cli/releases/download/v2.0.0-beta.0/codew-v2.0.0-beta.0-x86_64-apple-darwin.tar.gz"
+  sha256 "018686d6b95af81c22bd07d48dbd3adbb7e13ad0b926ae9aa459a1f8cb5b04d3"
 
   def install
     bin.install "codew"

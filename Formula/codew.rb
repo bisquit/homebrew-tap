206fdf4ab427966f0c143065a16ae7cc3c9030ac
shuheikikuchi <qu1dpr09u0@gmail.com>
2024-02-01 20:34:43 +0900

    modify sha

--- Formula/codew.rb
+++ Formula/codew.rb
@@ -2,7 +2,7 @@ class Codew < Formula
   desc "Open folder as a vscode multi-root workspace"
   homepage "https://github.com/bisquit/codew-cli"
   url "https://github.com/bisquit/codew-cli/releases/download/v2.0.0-beta.0/codew-v2.0.0-beta.0-x86_64-apple-darwin.tar.gz"
-  sha256 "018686d6b95af81c22bd07d48dbd3adbb7e13ad0b926ae9aa459a1f8cb5b04d3"
+  sha256 "4c34e9552dcc74f984181150eb5fe28984695eda922f189490584b223876a80a"
 
   def install
     bin.install "codew"
